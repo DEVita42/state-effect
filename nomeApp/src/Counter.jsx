39850d700e0a6c930006df824281a47a0389f3f8
DEVita42 <dvt.lorenzo@gmail.com>
2024-10-18 23:13:28 +0200

    state2

--- nomeApp/src/Counter.jsx
+++ nomeApp/src/Counter.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+import CounterDisplay from "./CounterDisplay";
 
 const Counter = () => {
-    const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-    function handleCounter() {
-        setCounter(counter + 1)
-    }
+  function handleCounterIncrement() {
+    setCounter(counter + 1);
+  }
+
+  function handleCounterDecrement() {
+    setCounter(counter - 1);
+  }
+
+  function handleCounterReset() {
+    setCounter(0);
+  }
 
   return (
     <div>
-      <h2>counter to {counter}</h2>
-      <button onClick={handleCounter}>increment</button>
+      <CounterDisplay counter={counter}/>
+      <button onClick={handleCounterDecrement}>decrement</button>
+      <button onClick={handleCounterReset}>reset</button>
+      <button onClick={handleCounterIncrement}>increment</button>
     </div>
   );
 };
---
+++ nomeApp/src/CounterDisplay.jsx
@@ -0,0 +1,9 @@
+function CounterDisplay({counter}) {
+
+    return (
+      <h2>counted to {counter}</h2>
+    )
+  }
+  
+  export default CounterDisplay
+  
